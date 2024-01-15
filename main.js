6eff3b266608702c0b4aaf10364e12f008f7d83d
t-coffe <tcoffe@edenschool.fr>
2024-01-15 14:41:31 +0100

    animation completed

--- main.js
+++ main.js
@@ -3,14 +3,32 @@ import './style.css';
 const cards = document.querySelectorAll('.card');
 let user_returned = 0;
 let game_returned = 0;
-let flag  ;
+/* let flag =  ; */
+let returnPossible = true ;
 
 for (let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', ()=> {
-        console.log(cards[i])
-        /* if (user_returned < 2 && flag != cards[i].data_key) {
-            
-        } */
-        cards[i].classList.remove("returned");
-    })
+    if (returnPossible) {
+        cards[i].addEventListener('click', ()=> {
+            console.log(cards[i]);
+            returningCard(cards[i]);
+            if (user_returned < 2 && cards[i].dataset.id != flag) {
+                
+            }
+            cards[i].classList.remove("returned");
+            user_returned ++;
+            if (user_returned = 2 ) {
+                returnPossible = false;
+            }
+            console.log(user_returned);
+        })
+    }
+}
+
+/* cards[i].dataset.id */
+
+function returningCard(cardToReturn) {
+    cardToReturn.classList.add('returning');
+    setTimeout(() => {
+        cardToReturn.classList.remove('returning','returned');
+    }, 500);
 }
