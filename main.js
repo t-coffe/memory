cec7d9d64a88f1e52732bd90aa3071390f24c27c
t-coffe <tcoffe@edenschool.fr>
2024-01-16 17:06:05 +0100

    function shuffling in progress of use, cards returned animation and winning if all the cards returned

--- main.js
+++ main.js
@@ -1,34 +1,64 @@
 import './style.css';
 
-const cards = document.querySelectorAll('.card');
-let user_returned = 0;
-let game_returned = 0;
-/* let flag =  ; */
-let returnPossible = true ;
+let userReturnedCard = 0;
+let gameReturnedCard = 0;
+let firstCard;
+let secondCard;
 
-for (let i = 0; i < cards.length; i++) {
-    if (returnPossible) {
-        cards[i].addEventListener('click', ()=> {
-            console.log(cards[i]);
-            returningCard(cards[i]);
-            if (user_returned < 2 && cards[i].dataset.id != flag) {
-                
-            }
-            cards[i].classList.remove("returned");
-            user_returned ++;
-            if (user_returned = 2 ) {
-                returnPossible = false;
-            }
-            console.log(user_returned);
-        })
-    }
+const cards = document.querySelectorAll('.card');
+
+function shuffleArray(arr) {
+    let result = [];
+    while (arr.length > 0) {
+        randomIndex = math.floor(maths.random()*arr.length);
+        result.push(arr[randomIndex]);
+        arr = arr.slice(0,randomIndex).concat(arr.slice(randomIndex+1));
+        return result;
+    } 
 }
 
-/* cards[i].dataset.id */
 
-function returningCard(cardToReturn) {
-    cardToReturn.classList.add('returning');
-    setTimeout(() => {
-        cardToReturn.classList.remove('returning','returned');
-    }, 500);
+for (let i = 0; i < cards.length; i++) {
+    const element = cards[i];
+    element.addEventListener('click', ()=> {
+        if (gameReturnedCard === 16) {
+            console.log('you finished the game, good job!');
+            return;
+        }
+        if (userReturnedCard === 0 ) {
+            firstCard = element;
+            element.classList.add('visible');
+            element.classList.remove('hidden');
+            userReturnedCard ++;
+            return;
+        }
+        if (userReturnedCard === 1) {
+            secondCard = element;
+            if (secondCard === firstCard) {
+                console.log('same card clicked');
+                return;
+            }
+            element.classList.add('visible');
+            element.classList.remove('hidden');
+            userReturnedCard ++;
+            if (firstCard.dataset.indexNumber === secondCard.dataset.indexNumber) {
+                console.log('same card');
+                userReturnedCard = 0;
+                gameReturnedCard +=2;
+            }else {
+                setTimeout(() => {
+                    firstCard.classList.remove('visible');
+                    secondCard.classList.remove('visible');
+                    firstCard.classList.add('hidden');
+                    secondCard.classList.add('hidden');
+                    userReturnedCard = 0;
+                }, 1000);
+                
+                console.log('they are not the same');
+                
+            }
+            return;
+        }        
+        
+    })
 }
