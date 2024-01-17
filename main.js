20378c1cd7176464334b97aa73019b91be2cd550
t-coffe <tcoffe@edenschool.fr>
2024-01-17 16:13:58 +0100

    function completed

--- main.js
+++ main.js
@@ -6,28 +6,46 @@ let firstCard;
 let secondCard;
 
 const cards = document.querySelectorAll('.card');
+const start = document.querySelector('button');
+const span = document.querySelector('span');
 
 function shuffleArray(arr) {
     let result = [];
     while (arr.length > 0) {
-        randomIndex = math.floor(maths.random()*arr.length);
+        const randomIndex = Math.floor(Math.random()*arr.length);
         result.push(arr[randomIndex]);
         arr = arr.slice(0,randomIndex).concat(arr.slice(randomIndex+1));
-        return result;
     } 
+    return result;
 }
+
+function cardsShuffle(arr) {
+    
+}
+
+start.addEventListener('click', ()=>{
+    userReturnedCard = 0;
+    gameReturnedCard = 0;
+    
+    const shuffledArr = shuffleArray([1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]);
+
+    for (let j = 0; j < cards.length; j++) {
+        cards[j].classList.remove('visible');
+        cards[j].textContent = shuffledArr[j];
+        cards[j].setAttribute('data-index-number', shuffledArr[j])
+    }
+});
 
 for (let i = 0; i < cards.length; i++) {
     const element = cards[i];
     element.addEventListener('click', ()=> {
-        if (gameReturnedCard === 16) {
-            console.log('you finished the game, good job!');
+        if (gameReturnedCard === cards.length) {
+            alert('you finished the game, good job!');
             return;
         }
         if (userReturnedCard === 0 ) {
             firstCard = element;
             element.classList.add('visible');
-            element.classList.remove('hidden');
             userReturnedCard ++;
             return;
         }
@@ -38,7 +56,6 @@ for (let i = 0; i < cards.length; i++) {
                 return;
             }
             element.classList.add('visible');
-            element.classList.remove('hidden');
             userReturnedCard ++;
             if (firstCard.dataset.indexNumber === secondCard.dataset.indexNumber) {
                 console.log('+ 1 duo');
@@ -48,8 +65,6 @@ for (let i = 0; i < cards.length; i++) {
                 setTimeout(() => {
                     firstCard.classList.remove('visible');
                     secondCard.classList.remove('visible');
-                    firstCard.classList.add('hidden');
-                    secondCard.classList.add('hidden');
                     userReturnedCard = 0;
                 }, 1000);
                 
